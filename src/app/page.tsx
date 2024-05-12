45a20846e7ee95aa5d2ace7d2eddcb675f7a26c9
Fenton Treasure <fentontreasure@gmail.com>
2024-05-12 17:54:09 +0100

    feat(landing): added upload section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import FiveStarRating from "@/components/FiveStarRating";
 import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
-import { Check, Star } from "lucide-react";
+import { ArrowRight, Check } from "lucide-react";
 import CustomerReviews from "@/components/CustomerReviews";
 import Reviews from "@/components/Reviews";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -94,6 +96,13 @@ function LandingSection() {
 }
 
 function HeroSection() {
+  const reasonsToBuy = [
+    "High-quality durable material",
+    "Scratch and fingerprint resistant coating",
+    "Wireless charging compatible",
+    "5 year print warrenty",
+  ];
+
   return (
     <>
       <section className="bg-slate-100 py-24">
@@ -135,6 +144,43 @@ function HeroSection() {
               </h2>
             </div>
           </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+              <img
+                src="/arrow.png"
+                alt="arrow image"
+                className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
+              />
+              <div className="relateive h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
+                <img
+                  src="/horse.jpg"
+                  alt="image of a horse"
+                  className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"
+                />
+              </div>
+              <Phone className="w-60" imgSrc="/horse_phone.jpg" />
+            </div>
+          </div>
+          <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+            {reasonsToBuy.map((reason, index) => (
+              <li key={reason.split(" ")[0] + index} className="w-fit">
+                <Check className="size-5  text-green-600 inline mr-1.5" />
+                {reason}
+              </li>
+            ))}
+            <div className="flex justify-center">
+              <Link
+                className={buttonVariants({
+                  size: "lg",
+                  className: "mx-auto mt-8",
+                })}
+                href="/configure/upload"
+              >
+                Create your case now <ArrowRight className="size-4 ml-1.5" />
+              </Link>
+            </div>
+          </ul>
         </MaxWidthWrapper>
       </section>
     </>
